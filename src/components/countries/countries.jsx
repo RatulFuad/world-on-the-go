b0a9b653b74e5e5723a146b1ba958435fc215072
Ratul Fuad <rtlkakakaku@gmail.com>
2025-09-22 19:52:58 +0600

    first commit

---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+
+            <h1>in the country....</h1>
+            
+        </div>
+    );
+};
+
+export default countries;
